1067b019d7767785cba0d558062b14ac6d10f464
migromarj <miguelromeroarjona@gmail.com>
2024-03-14 12:18:52 +0100

    Remove unused repository

--- src/api/containers/container.ts
+++ src/api/containers/container.ts
@@ -1,6 +1,4 @@
-import { createContainer, asValue, asClass } from 'awilix'
-
-import ModelRepository from '../repositories/ModelRepository'
+import { createContainer, asClass } from 'awilix'
 
 import ModelService from '../services/ModelService'
 import OllamaModelService from '../services/OllamaModelService'
@@ -9,7 +7,6 @@ function initContainer() {
     const container = createContainer()
 
     container.register({
-        modelRepository: asValue(ModelRepository),
         modelService: asClass(ModelService).singleton(),
         ollamaModelService: asClass(OllamaModelService).singleton(),
     })
