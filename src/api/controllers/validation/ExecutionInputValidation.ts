2c30edb28aabbb5d14ef43da4f7260c5cc02ddc5
migromarj <migromarj@alum.us.es>
2024-06-07 12:30:11 +0200

    Update postman collection examples

--- src/api/controllers/validation/ExecutionInputValidation.ts
+++ src/api/controllers/validation/ExecutionInputValidation.ts
@@ -20,7 +20,7 @@ const execute = [
         .isLength({ min: 1, max: 30 })
         .trim()
         .withMessage(
-            'modelName is optional but must be a string with length between 1 and 30 if provided'
+            'model_name is optional but must be a string with length between 1 and 30 if provided'
         ),
     check('excluded_text')
         .optional()
@@ -28,25 +28,25 @@ const execute = [
         .isLength({ min: 0, max: 30 })
         .trim()
         .withMessage(
-            'excludedText is optional but must be a string with length between 0 and 30 if provided'
+            'excluded_text is optional but must be a string with length between 0 and 30 if provided'
         ),
     check('response_max_length')
         .optional()
         .isInt({ min: 1, max: 2000 })
         .withMessage(
-            'responseMaxLength is optional but must be an integer between 1 and 2000 if provided'
+            'response_max_length is optional but must be an integer between 1 and 2000 if provided'
         ),
     check('list_format_response')
         .optional()
         .isBoolean()
         .withMessage(
-            'listFormatResponse is optional but must be a boolean if provided'
+            'list_format_response is optional but must be a boolean if provided'
         ),
     check('exclude_bias_references')
         .optional()
         .isBoolean()
         .withMessage(
-            'excludeBiasReferences is optional but must be a boolean if provided'
+            'exclude_bias_references is optional but must be a boolean if provided'
         ),
 ]
 
